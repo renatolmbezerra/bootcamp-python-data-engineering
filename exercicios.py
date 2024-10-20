c17a3ce8cf783400fa2eae18c25a828a2b0d7061
Renato-Tecpel <renatolmb@hotmail.com>
2024-10-20 00:07:32 -0300

    Conclusao dos exercicios de strings

--- exercicios.py
+++ exercicios.py
@@ -63,12 +63,27 @@ print(f"{area:.2f}")
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
-
+texto = input("Escreva um texto: ")
+print(texto.upper())
 
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+nome = input("Digite seu nome completo: ")
+nome = nome.lower()
+print(nome)
+
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+frase = input("Escreve uma frase: ")
+print(frase.strip())
+
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+data = input("Digite uma data no formato dd/mm/aaaa: ")
+print(data.split("/"))
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
+str1 = input("Digite a primeira palavra: ")
+str2 = input("Digite a segunda palavra: ")
+juncao = str1 + " " + str2
+print(juncao)
 
 # #### Booleanos (`bool`)
 
