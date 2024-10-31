f18dd20980c1158261fc97fe132db020dc1c3e40
Renato Macedo <renatolmb@hotmail.com>
2024-10-31 19:41:14 -0300

    Descrição das mudanças locais

--- exercicios.py
+++ exercicios.py
@@ -1,13 +1,12 @@
 # Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
-numeros = list(range(0,11))
+numeros = list(range(0, 11))
 for numero in numeros:
     print(numero**2)
 
 
 # Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
 lista = ["Python", "Java", "C++", "JavaScript"]
-lista.pop(2)
-print(lista)
+lista.remove("C++")
 lista.append("Ruby")
 print(lista)
 
@@ -17,6 +16,22 @@ livro: dict = {
     "autor": "Fulano",
     "ano": 2017
 }
+for chave, valor in livro.items():
+    print(f"{chave}: {valor}")
 
 # Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
+def contar_caracteres(s):
+    contagem = {}
+    for caractere in s:
+        contagem[caractere] = contagem.get(caractere, 0) + 1
+    return contagem
+
+print(contar_caracteres("engenharia de dados"))
+
 # Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
+lista_compras = ["maçã", "banana", "cereja"]
+precos = {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}
+total = sum(precos[item] for item in lista_compras)
+print(f"Preço total: {total}")
+
+
