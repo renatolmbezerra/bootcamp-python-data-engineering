c33ce28eb67532b6007d45bb451b2402358a2cc0
Renato Macedo <renatolmb@hotmail.com>
2024-10-31 21:00:01 -0300

    Resolução parcial do exercicio 91

--- exercicios.py
+++ exercicios.py
@@ -1,4 +1,4 @@
-import random as rd
+import random 
 
 # # Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
 # numeros = list(range(0,11))
@@ -20,39 +20,61 @@ import random as rd
 #     "ano": 2017
 # }
 
-# Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
-def contar_caracteres(s):
-    contagem = {}
-    for caractere in s:
-        contagem[caractere] = contagem.get(caractere, 0) + 1
-    return contagem
+# # Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
+# def contar_caracteres(s):
+#     contagem = {}
+#     for caractere in s:
+#         contagem[caractere] = contagem.get(caractere, 0) + 1
+#     return contagem
 
-print(contar_caracteres("engenharia de dados"))
+# print(contar_caracteres("engenharia de dados"))
 
-# Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
-lista_compras = ["maçã", "banana", "cereja"]
-precos = {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}
-total = sum(precos[item] for item in lista_compras)
-print(f"Preço total: {total}")
+# # Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
+# lista_compras = ["maçã", "banana", "cereja"]
+# precos = {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}
+# total = sum(precos[item] for item in lista_compras)
+# print(f"Preço total: {total}")
 
-# 90.Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
-# No final, mostre o conteúdo da estrutura na tela.
+# # 90.Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
+# # No final, mostre o conteúdo da estrutura na tela.
 
-aluno = {}
+# aluno = {}
 
-aluno["Nome"] = input("Nome: ")
-aluno["Média"] = float(input(f"Média de {aluno["Nome"]}: "))
-if aluno["Média"] < 7:
-    aluno["Situacao"] = "Reprovado"
-else:
-    aluno["Situacao"] = "Aprovado"
+# aluno["Nome"] = input("Nome: ")
+# aluno["Média"] = float(input(f"Média de {aluno["Nome"]}: "))
+# if aluno["Média"] < 7:
+#     aluno["Situacao"] = "Reprovado"
+# else:
+#     aluno["Situacao"] = "Aprovado"
 
-for k, v in aluno.items():
-    print(f"{k} é igual a {v}")
+# for k, v in aluno.items():
+#     print(f"{k} é igual a {v}")
 
 # 91. Crie um programa onde 4 jogadores joguem um dado e tenham resultdos aleatórios.
 # Guarde esses resultados em um dicionário. No final, coloque esse dicionário em ordem, sabendo 
 # que o vencedor tirou o maior número no dado.
 
-print(rd.randint)
+jogo = {}
+
+jogo["jogador1"] = random.randint(1, 6)
+jogo["jogador2"] = random.randint(1, 6)
+jogo["jogador3"] = random.randint(1, 6)
+jogo["jogador4"] = random.randint(1, 6)
+
+print("Valores sorteados:")
+for k, v in jogo.items():
+    print(f"    O {k} tirou {v}")
+
+print("Ranking dos jogadores:")
+
+# Sort based on Values
+val_based = {k: v for k, v in sorted(jogo.items(), key=lambda item: item[1])}
+# item[1] represents the sorting based on value
+
+# Sort based on reverse of Values
+val_based_rev = {k: v for k, v in sorted(jogo.items(), key=lambda item: item[1], reverse=True)}
+
+# Print sorted dictionary
+print(val_based)
+print(val_based_rev)
 
