aec7ff47e49e5784fcf4c0351583ef559deb6702
Renato Macedo <renatolmb@hotmail.com>
2024-12-15 16:19:51 -0300

    Projeto finalizado

--- etl.py
+++ etl.py
@@ -13,7 +13,21 @@ def ler_csv(nome_do_arquivo_csv: str) -> list[dict]:
             lista.append(linha)
     return lista
 
+def filtrar_produtos_nao_entregues(lista: list[dict]) -> list[dict]:
+    """
+    Funcao que filtra produtos onde entrega = True
+    """
+    lista_com_produtos_filtrados = []
+    for produto in lista:
+        if produto.get("entregue") == "True":
+            lista_com_produtos_filtrados.append(produto)
+    return lista_com_produtos_filtrados
 
-vendas_itens: list[dict]
-vendas_itens = ler_csv(path_arquivo)
-print(vendas_itens)
+def somar_valores_dos_produtos(lista_com_produtos_filtrados: list[dict]) -> float:
+    """
+    Soma todos os produtos que estão na lista
+    """
+    valor_total = 0
+    for produto in lista_com_produtos_filtrados:
+        valor_total += float(produto.get("price"))
+    return valor_total
