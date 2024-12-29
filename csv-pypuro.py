202dc590133bf19c06295c3ff34c247ec42f852c
Renato Macedo <renatolmb@hotmail.com>
2024-12-29 17:01:33 -0300

    Imnplementando classe com recursão

--- csv-pypuro.py
+++ csv-pypuro.py
@@ -3,10 +3,7 @@ import csv
 path = './exemplo.csv'
 
 with open(path, mode="r", encoding="utf-8") as arquivo_csv:
-    leitor = csv.reader(arquivo_csv)
-
-    cabecalho = next(leitor)
-    print(cabecalho)
+    leitor = csv.DictReader(arquivo_csv)
 
     for linha in leitor:
         print(linha)
